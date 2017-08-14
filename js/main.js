6f0f867662ba5dcd093d01e8e75e29cbf2275037
Daniel Chen <danielchenphhs@gmail.com>
2017-08-14 11:30:26 -0400

    vis done

--- js/main.js
+++ js/main.js
@@ -7,40 +7,146 @@ var w = window,
     g = d.getElementsByTagName('body')[0],
     x, y, ypos, ybot;
 
+checkWinStat()
 
-function checkBotPos() {
+function checkWinStat() {
     x = w.innerWidth || e.clientWidth || g.clientWidth;
     y = w.innerHeight|| e.clientHeight|| g.clientHeight;
     ypos = w.scrollY;
-    return ypos + y - 200;
-
 }
 
-var data;
+function checkBotPos() {
+    checkWinStat();
+    return ypos + y - 300;
+}
 
-d3.csv("data/scroll_interp.csv", function(d){
-    data = d;
-
-    function updatePixelCount() {
-        var pos = checkBotPos();
-        document.getElementById('counterright').innerHTML = pos.toString() + " px";
-        if (pos > 3000) {
-            document.getElementById('counterleft').innerHTML = data[3000].percent.toString() + " % readers";
-        } else if (pos < 0) {
-            document.getElementById('counterleft').innerHTML = data[0].percent.toString() + " % readers";
-        } else {
-            document.getElementById('counterleft').innerHTML = data[pos].percent.toString() + " % readers";
-        }
+function updatePixelCount() {
+    var pos = checkBotPos();
+    document.getElementById('pixels').innerHTML = pos.toString() + "px";
+    if (pos > 3000) {
+        document.getElementById('percent').innerHTML = data[3000].percent.toString() + "%";
+    } else if (pos < 0) {
+        document.getElementById('percent').innerHTML = data[0].percent.toString() + "%";
+    } else {
+        document.getElementById('percent').innerHTML = data[pos].percent.toString() + "%";
     }
+}
+
+$( window ).resize(function() {
+    updateVis();
+});
+$( window ).scroll(function() {
+    updateVis();
+});
+
+var data;
+var numPeople; // may scale based on window size
+var people = [];
+var svg;
+
+// for (var i = 0; i < numPeople; i++) {
+//     var person = {
+//         active: true
+//     };
+//     people.push(person);
+// }
+
+
+queue()
+    .defer(d3.csv,"data/scroll_interp.csv")
+    .await(initVis);
+
+function initVis(error, d){
+    if(error) { console.log(error); }
+
+    data = d;
+    data.pixel = +data.pixel;
+    data.percent = +data.percent;
+
+    svg = d3.select("#people-vis").append("svg")
+        .attr("width", x)
+        .attr("height", 200)
+        .attr("transform", "translate(0," + 70 + ")")
+    ;
+
+    updateVis();
+}
+
+function updateVis() {
 
     updatePixelCount();
 
-    $( window ).resize(function() {
-        updatePixelCount();
-    });
-    $( window ).scroll(function() {
-        updatePixelCount();
-    });
+    var pos = checkBotPos();
 
-    console.log(data);
-});
+    var people = [];
+
+    if (x > 800) {
+        numPeople = 20;
+    } else {
+        numPeople = 10;
+    }
+
+    for (var i = 0; i < numPeople; i++) {
+        var person = {
+            active: true
+        };
+        people.push(person);
+    }
+
+    svg.attr("width",x);
+
+    var bg_image = svg.selectAll(".bg-image")
+        .data(people);
+
+    // enter
+    bg_image.enter()
+        .append("image")
+        .attr("class", "bg-image");
+
+    // update
+    bg_image
+        .attr("width", 40)
+        .attr("transform", function(d, i) {
+            return "translate(" + ((x / (numPeople + 6)) * (i + 3) - 20) + "," + "0" + ")";
+        })
+        .attr("xlink:href", "img/grayperson.svg")
+        ;
+
+    // exit
+    bg_image.exit()
+        .remove();
+
+
+    var image = svg.selectAll(".image")
+        .data(people);
+
+    // enter
+    image.enter()
+        .append("image")
+        .attr("class", "image")
+        .attr("width", 40)
+        .attr("xlink:href", "img/pinkperson.svg")
+    ;
+
+    // update
+    image
+        .attr("transform", function(d, i) {
+            return "translate(" + ((x / (numPeople + 6)) * (i + 3) - 20) + "," + "0" + ")";
+        })
+        .transition(100)
+        .attr("opacity", function(d,i) {
+            if (pos > 3000) {
+                return 0;
+            } else if (i < Math.round(data[pos].percent * numPeople / 100)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+    // exit
+    image.exit()
+        .remove();
+
+
+}
